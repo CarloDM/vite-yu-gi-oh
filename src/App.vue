00ab48662e12b1eb6bc2326125bbcc96b0b168ae
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-02 15:52:58 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Header from './components/main.vue';
+import Main from './components/main.vue';
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Main,
+  }
+
+}
+</script>
+
+<template>
+
+  <Header/>
+
+  <Main />
+
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,19 @@
+
+<script>
+import Card from './card.vue'
+export default {
+  name:'Main',
+  components:{
+    Card,
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+
+<style>
+
+</style>
